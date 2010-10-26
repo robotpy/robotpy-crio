6751a56b24743f7c9cf84a9c4193ede3ab991382
Peter Johnson <peter@tortall.net>
2010-10-25 18:29:15 -0700

    make_dist.bat: The user program is called FRC_UserProgram.out.

--- make_dist.bat
+++ make_dist.bat
@@ -9,6 +9,6 @@ copy README.txt dist\
 xcopy /i /e py dist\robot\py
 xcopy /i /e Python\Lib dist\robot\lib\python3.1
 xcopy /i /e samples dist\samples
-copy PPC603gnu\RobotPy\NonDebug\RobotPy.out dist\robot\ni-rt\system\UserProgram.out
+copy PPC603gnu\RobotPy\NonDebug\RobotPy.out dist\robot\ni-rt\system\FRC_UserProgram.out
 
 pause
