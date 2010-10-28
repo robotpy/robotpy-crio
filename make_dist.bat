c0977ada032e7bb28328cd090c5beb55c166472e
Peter Johnson <peter@tortall.net>
2010-10-27 22:36:06 -0700

    make_dist.bat: Copy binary modules into lib\python3.1\dyn-load.

--- make_dist.bat
+++ make_dist.bat
@@ -11,4 +11,7 @@ xcopy /i /e RobotPy\Python\Lib dist\robot\lib\python3.1
 xcopy /i /e samples dist\samples
 copy RobotPy\PPC603gnu\RobotPy\NonDebug\RobotPy.out dist\robot\ni-rt\system\FRC_UserProgram.out
 
+mkdir dist\robot\lib\python3.1\dyn-load
+for /r Modules %%X in (*.out) do (copy %%X dist\robot\lib\python3.1\dyn-load)
+
 pause
