d784f16b4a34237eb51181122747409cfa971277
Peter Johnson <peter@tortall.net>
2010-10-31 14:58:17 -0700

    Split distributions into Core and WPILib portions.

--- make_dist.bat
+++ make_dist.bat
@@ -1,20 +1,31 @@
 rmdir /s /q dist
 mkdir dist
-mkdir dist\robot
-mkdir dist\robot\lib
-mkdir dist\robot\ni-rt
-mkdir dist\robot\ni-rt\system
 
-copy README.txt dist\
-xcopy /i /e py dist\robot\py
-xcopy /i /e RobotPy\Python\Lib dist\robot\lib\python3.1
-xcopy /i /e samples dist\samples
-copy RobotPy\PPC603gnu\RobotPy\NonDebug\RobotPy.out dist\robot\ni-rt\system\FRC_UserProgram.out
+rem RobotPy-Core distribution
+mkdir dist\RobotPy-Core
+mkdir dist\RobotPy-Core\robot
+mkdir dist\RobotPy-Core\robot\lib
+mkdir dist\RobotPy-Core\robot\ni-rt
+mkdir dist\RobotPy-Core\robot\ni-rt\system
 
-mkdir dist\robot\lib\python3.1\dyn-load
-for /r Modules %%X in (*.out) do (copy %%X dist\robot\lib\python3.1\dyn-load)
+copy README.txt dist\RobotPy-Core\
+xcopy /i /e py dist\RobotPy-Core\robot\py
+xcopy /i /e RobotPy\Python\Lib dist\RobotPy-Core\robot\lib\python3.1
+copy RobotPy\PPC603gnu\RobotPy\NonDebug\RobotPy.out dist\RobotPy-Core\robot\ni-rt\system\FRC_UserProgram.out
 
-for /r Packages %%X in (*.out) do (copy %%X dist\robot\lib\python3.1\site-packages)
-for /r Packages %%X in (*.py) do (copy %%X dist\robot\lib\python3.1\site-packages)
+mkdir dist\RobotPy-Core\robot\lib\python3.1\dyn-load
+for /r Modules %%X in (*.out) do (copy %%X dist\RobotPy-Core\robot\lib\python3.1\dyn-load)
+
+rem RobotPy-WPILib distribution
+mkdir dist\RobotPy-WPILib
+mkdir dist\RobotPy-WPILib\robot
+mkdir dist\RobotPy-WPILib\robot\lib
+mkdir dist\RobotPy-WPILib\robot\lib\python3.1
+mkdir dist\RobotPy-WPILib\robot\lib\python3.1\site-packages
+
+xcopy /i /e samples dist\RobotPy-WPILib\samples
+
+for /r Packages\wpilib %%X in (*.out) do (copy %%X dist\RobotPy-WPILib\robot\lib\python3.1\site-packages)
+for /r Packages\wpilib %%X in (*.py) do (copy %%X dist\RobotPy-WPILib\robot\lib\python3.1\site-packages)
 
 pause
