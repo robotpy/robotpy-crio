2d64b8d87c5d847ca35a4f23c4eb467a837da95e
Dustin Spicuzza <dustin@virtualroadside.com>
2014-01-27 22:59:39 -0500

    Write the wpilib version to disk, as RobotBase is supposed to do

--- Packages/wpilib/package_wpilib/__init__.py
+++ Packages/wpilib/package_wpilib/__init__.py
@@ -2,3 +2,9 @@ from .core import *
 from _wpilib import  __version__
 
 print("WPILib was compiled from revision %s" % __version__)
+
+try:
+    with open('/c/FRC_Lib_Version.ini', 'w') as fp:
+        fp.write(__version__)
+except:
+    print("!! Exception occurred when writing library version to disk")
